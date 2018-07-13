51a88c654ebc19b69ec1fcec36de4563ac4459a6
vagrant <vagrant@homestead>
2018-07-13 14:08:51 +0000

    add admin styles

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,16 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+  return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
+Route::get('/admin', function() {
+  return view('admin.index');
+});
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('admin/users', 'AdminUsersController');
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,17 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.js([
+  'resources/assets/js/libs/bootstrap.min.js',
+  'resources/assets/js/libs/jquery.js',
+  'resources/assets/js/libs/metisMenu.js',
+  'resources/assets/js/libs/sb-admin-2.js',
+  'resources/assets/js/libs/scripts.js'
+], 'public/js/app.js')
+.styles([
+  'resources/assets/css/libs/blog-post.css',
+  'resources/assets/css/libs/font-awesome.css',
+  'resources/assets/css/libs/metisMenu.css',
+  'resources/assets/css/libs/sb-admin-2.css',
+  'resources/assets/css/libs/bootstrap.css'
+], 'public/css/app.css');
